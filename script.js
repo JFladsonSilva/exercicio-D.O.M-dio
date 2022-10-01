dd68cd52f1e64b86ac66366ca3e8fcbab92c972e
JFladsonSIlva <fladsonsilva1@gmail.com>
2022-10-01 20:01:53 -0300

    pausa na construção do js

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeMode(){
+    
+}
+
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const footer = document.getElementsByTagName('footer')[0];
+const body = document.getElementsByTagName('body')[0];
+
+button.addEventListener('click', changeMode);
+
+console.log(footer); 
